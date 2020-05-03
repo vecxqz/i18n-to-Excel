92391b6e3058e1297a1d322eb5c4eae936fef3a0
vecxqz <741621270@qq.com>
2020-05-03 23:52:17 +0800

    chore: 更新webpack配置文件

--- web/config/webpack.prod.js
+++ web/config/webpack.prod.js
@@ -1,6 +1,10 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
   mode: 'production',
 });
